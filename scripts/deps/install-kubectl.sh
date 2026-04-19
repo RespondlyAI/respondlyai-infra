49be21cabf9cf433fdb5747fe359780f887172ed
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-04-20 03:25:32 +0530

    feat: install scripts for infra deps

--- scripts/deps/install-kubectl.sh
+++ scripts/deps/install-kubectl.sh
@@ -8,27 +8,3 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Clean up the downloaded file
 rm kubectl
-#!/bin/bash
-
-# Configuration (3 CPUs, 12GB RAM)
-CPUS=3
-MEMORY=12288
-
-echo -e "\033[1;36m==================================================\033[0m"
-echo -e "\033[1;32m     BOOTING KUBERNETES CLUSTER (MINIKUBE)\033[0m"
-echo -e "\033[1;36m==================================================\033[0m"
-
-# Boot the cluster using the variables
-echo -e "\033[1;33m[1/3] Provisioning Control Plane and Worker Nodes...\033[0m"
-minikube start --cpus=$CPUS --memory=$MEMORY --driver=docker
-
-# Enable essential addons
-echo -e "\033[1;33m[2/3] Enabling Nginx Ingress Controller...\033[0m"
-minikube addons enable ingress > /dev/null
-
-echo -e "\033[1;33m[3/3] Verifying Cluster Health...\033[0m"
-kubectl get nodes
-
-echo -e "\n\033[1;32m✅ KUBERNETES IS READY\033[0m"
-echo -e "You can now apply your YAML manifests."
-echo -e "\033[1;36m==================================================\033[0m"
