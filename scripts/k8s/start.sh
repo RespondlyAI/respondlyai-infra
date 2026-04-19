ce0ae5965613a84210c2d68f2ab70124284ab4ae
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-04-20 03:27:53 +0530

    feat: dynamic cpu and memory allocation from script

--- scripts/k8s/start.sh
+++ scripts/k8s/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Configuration
-CPUS=2
-MEMORY=8192
+CPUS=3
+MEMORY=12288
 
 echo -e "\033[1;36m==================================================\033[0m"
 echo -e "\033[1;32m     BOOTING KUBERNETES CLUSTER (MINIKUBE)\033[0m"
@@ -10,7 +10,7 @@ echo -e "\033[1;36m==================================================\033[0m"
 
 # Boot the cluster with specific guardrails
 echo -e "\033[1;33m[1/3] Provisioning Control Plane and Worker Nodes...\033[0m"
-minikube start --cpus=2 --memory=8192 --driver=docker
+minikube start --cpus=$CPUS --memory=$MEMORY --driver=docker
 
 # Enable essential addons
 echo -e "\033[1;33m[2/3] Enabling Nginx Ingress Controller...\033[0m"
